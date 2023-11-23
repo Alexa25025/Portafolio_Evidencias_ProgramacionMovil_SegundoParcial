7cfb6e244ddf15df41f37397a73c8636a3cfa40c
Wendy Alexandra <144569869+Alexa25025@users.noreply.github.com>
2023-11-22 19:56:04 -0600

    Delete src/main/kotlin directory

--- src/main/kotlin/Clases/Goomba.kt
+++
@@ -1,7 +0,0 @@
-package Clases
-
-class Goomba: Enemy("Goomba", 1){
-    init {
-        println("Iniciando subclase de $name")
-    }
-}
--- src/main/kotlin/Clases/Movie.kt
+++
@@ -1,5 +0,0 @@
-package Clases
-
-data class Movie(val name: String, val gender: String, val duration: Double) {
-    var createdAt = ""
-}
